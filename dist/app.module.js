c44af2f018e194b41efc85dbb00b08601cd81cab
Inaku Joel <inakujoel16@gmail.com>
2021-08-25 14:00:01 +0100

    chore: code cleanup

--- dist/app.module.js
+++ dist/app.module.js
@@ -23,7 +23,7 @@ AppModule = __decorate([
                 type: 'postgres',
                 autoLoadEntities: true,
                 entities: [],
-                url: 'postgres://kdsebwidssgndo:98ec89114a5a1e52d965072941d49427995f9ae201533bb1a9be08442b2e2e76@ec2-35-174-122-153.compute-1.amazonaws.com:5432/d23q0n9iu3166p',
+                url: 'postgres://ynjkxwlyualiag:3175d2e205a1334974ee4c9b3bec37e50b36bfe2e88ab1bb28cb4ac092b08000@ec2-54-147-93-73.compute-1.amazonaws.com:5432/deio4tc1l9pdlb',
                 synchronize: true
             }), post_module_1.PostModule
         ],
